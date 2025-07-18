3806e8cfe018f0546a3077fd95c8161ed4f68b54
sasakiyasp <sasakiyasp@systena.co.jp>
2025-07-18 11:50:36 +0900

    Terraform oke variables

--- providers.tf
+++ providers.tf
@@ -1,16 +1,16 @@
 provider "oci" {
-  fingerprint      = ${{ secrets.OCI_CLI_FINGERPRINT }}
-  private_key_path = "/home/sasakiyasp/.oci/oci_api_key.pem"
-  region           = ${{ secrets.OCI_CLI_REGION }}
-  tenancy_ocid     = ${{ secrets.OCI_CLI_TENANCY }}
-  user_ocid        = ${{ secrets.OCI_CLI_USER }}
+  fingerprint      = OCI_CLI_FINGERPRINT
+  private_key_path = OCI_CLI_KEYPATH
+  region           = OCI_CLI_REGION
+  tenancy_ocid     = OCI_CLI_TENANCY
+  user_ocid        = OCI_CLI_USER
 }
 
 provider "oci" {
-  fingerprint      = ${{ secrets.OCI_CLI_FINGERPRINT }}
-  private_key_path = "/home/sasakiyasp/.oci/oci_api_key.pem"
-  region           = ${{ secrets.OCI_CLI_REGION }}
-  tenancy_ocid     = ${{ secrets.OCI_CLI_TENANCY }}
-  user_ocid        = ${{ secrets.OCI_CLI_USER }}
+  fingerprint      = OCI_CLI_FINGERPRINT
+  private_key_path = OCI_CLI_KEYPATH
+  region           = OCI_CLI_REGION
+  tenancy_ocid     = OCI_CLI_TENANCY
+  user_ocid        = OCI_CLI_USER
   alias            = "home"
 }
