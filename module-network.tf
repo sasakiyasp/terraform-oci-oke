7f357eebd8ca928b4b9d9879f751c4f083918aa0
sasakiyasp <sasakiyasp@systena.co.jp>
2025-07-18 16:51:39 +0900

    Terraform oke module-network modify

--- module-network.tf
+++ module-network.tf
@@ -36,7 +36,7 @@ locals {
     anytrue([                                             # enable for configurations that generally utilize it
       var.vcn_create_nat_gateway == "always",             # always enable
       !var.worker_is_public,                              # enable for private workers
-      var.create_operator,                                # enable for operator
+#      var.create_operator,                                # enable for operator
       !var.control_plane_is_public,                       # enable for cluster w/ private endpoint
       contains(["internal", "both"], var.load_balancers), # enable for cluster w/ private load balancers
     ])
@@ -54,12 +54,12 @@ module "vcn" {
 
   # Standard tags as defined if enabled for use, or freeform
   # User-provided tags are merged last and take precedence
-  defined_tags = merge(var.use_defined_tags ? {
-    "${var.tag_namespace}.state_id" = local.state_id,
-    "${var.tag_namespace}.role"     = "network",
-    } : {},
-    local.network_defined_tags,
-  )
+#  defined_tags = merge(var.use_defined_tags ? {
+#    "${var.tag_namespace}.state_id" = local.state_id,
+#    "${var.tag_namespace}.role"     = "network",
+#    } : {},
+#    local.network_defined_tags,
+#  )
 #  freeform_tags = merge(var.use_defined_tags ? {} : {
 #    "state_id" = local.state_id,
 #    "role"     = "network",
@@ -116,9 +116,9 @@ module "network" {
   state_id         = local.state_id
   compartment_id   = coalesce(var.network_compartment_id, local.compartment_id)
   defined_tags     = local.network_defined_tags
-  #freeform_tags    = local.network_freeform_tags
-  #tag_namespace    = var.tag_namespace
-  #use_defined_tags = var.use_defined_tags
+   # freeform_tags    = local.network_freeform_tags
+   # tag_namespace    = var.tag_namespace
+   # use_defined_tags = var.use_defined_tags
 
   allow_node_port_access       = var.allow_node_port_access
   allow_pod_internet_access    = var.allow_pod_internet_access
