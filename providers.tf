8658af590f38032718ff6bd90ac27d307defc96b
sasakiyasp <sasakiyasp@systena.co.jp>
2025-07-22 12:56:00 +0900

    Terraform oke plan fix（provate key）

--- providers.tf
+++ providers.tf
@@ -1,6 +1,7 @@
 provider "oci" {
   fingerprint      = "OCI_CLI_FINGERPRINT"
-  private_key_path = "OCI_CLI_KEYPATH"
+#  private_key_path = "OCI_CLI_KEYPATH"
+  private_key_content = "CLI_KEY_CONTENT"
   region           = "OCI_CLI_REGION"
   tenancy_ocid     = "OCI_CLI_TENANCY"
   user_ocid        = "OCI_CLI_USER"
