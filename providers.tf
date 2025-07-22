16a8727d6bc111688e42e2821684ec44be5a95ab
sasakiyasp <sasakiyasp@systena.co.jp>
2025-07-22 13:20:49 +0900

    fix

--- providers.tf
+++ providers.tf
@@ -1,7 +1,7 @@
 provider "oci" {
   fingerprint      = "OCI_CLI_FINGERPRINT"
   private_key_path = "OCI_CLI_KEYPATH"
-  export TF_VAR_private_key=`openssl rsa -in My_api_key.pem -check`
+  export TF_VAR_private_key =`openssl rsa -in My_api_key.pem -check`
   region           = "OCI_CLI_REGION"
   tenancy_ocid     = "OCI_CLI_TENANCY"
   user_ocid        = "OCI_CLI_USER"
